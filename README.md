49d65fbb3116c0be7dbd7b073462b3d3a1fe8298
demonzz2 <78615704+demonzz2@users.noreply.github.com>
2021-04-14 12:05:41 +0200

    'commit'

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 ## Installation
+
+**Jeśli twój nick posiada cyfry to skrypt nie będzie działał prawidłowo, lecz dalej będzie wysyłał wiadomości**
+
 0. Pobierz node https://nodejs.org/en/download/
 1. Utworz folder dla bota 
-2. Uruchom cmd, wpisz [cd (ścieżka do folderu)]`
+2. Uruchom cmd, wpisz `cd (ścieżka do folderu)`
 3. Nacisnij zielony przycisk z napisem [code] po prawej stronie ekranu, download ZIP. Przenieś pliki do folderu i wypakuj.
 4. Przechodzimy do folderu z botem za pomocą `cd demonzz1-bot`
 5. W cmd będac w folderze bota wpisujemy `npm i`
 6. Edytujemy plik **config.json** `instrukcje w pliku`
-8. Uruchamiamy bota komendą `node bot`"# demonzz1-bot" 
+8. Uruchamiamy bota komendą `node bot` 
---
+++ bot.js
@@ -0,0 +1,55 @@
+const config = require("./config.json")
+const tmi = require('tmi.js')
+
+const user = {
+    identity: {
+        username: config.username,
+        password: config.oauth
+    },
+    channels: [
+        'demonzz1',
+        'kryso___reformed',
+    ]
+}
+
+const client = new tmi.client(user);
+client.connect();
+
+let heist = config.heist
+
+// events
+client.on('message', (channel, tags, message) => {
+    if (tags['display-name'] != "demonzzbot") {
+        return;
+    }
+    else {
+        if(message.includes("Type !boss to join!")) {
+            client.say(channel, '!boss');
+            console.log("!boss")
+        }
+        if(message.includes("Type !ffa to join!")) {
+            client.say(channel, '!ffa')
+            console.log("!ffa")
+        }
+        if(message.includes("-Everyone can Join!- In order to join type !heist (amount).")) {
+            client.say(channel, `!heist ${heist}`)
+            console.log(`!heist ${heist}`)
+        }
+    }
+})
+
+client.on('message', (channel, tags, message) => {
+    if (tags['display-name'] != "demonzzbot") return
+    else {
+        if(message.includes("heist") && message.includes(user.identity.username)) {
+            console.log("Wygrałeś heist! Wygrane pkt: " + heist*2)
+        }
+        if(message.includes(`${user.identity.username} (`)) {
+            console.log("Wygrane pkt: " + message.match(`${user.identity.username} ( /\d+/)`))
+        }
+    } 
+})
+
+client.on('connected', () => {
+    console.log(`Połączono z ${user.channels}`)
+})
